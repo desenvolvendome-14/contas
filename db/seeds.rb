34a6e41c49efcfdbe557718b0e35e9c4673f1365
Lukas Pol <lukaspaespol@gmail.com>
2021-09-23 05:38:36 -0300

    chore: add seed for bill

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Bill.create!(bill_type: :invoice_receivable, company_id: 1, invoice: 'fatura a receber', type_invoice: :bill_safe,
+             issuance_date: Date.today, invoice_value: 150.99, increase: 0, salesman_id: 1, commission: 10)
+
+Bill.create!(bill_type: :invoice_payable, company_id: 2, invoice: 'fatura a pagar', type_invoice: :bill_safe,
+             issuance_date: Date.today, invoice_value: 290.99, increase: 0, scanning_date: Date.today,
+             description: 'Nada', account_plan_id: 1,cost_center_id: 2, discount: 0, net_value: 0)
+
+Bill.create!(bill_type: :expense_payable, company_id: 2, invoice: 'despesas a pagar', type_invoice: :bill_safe,
+             issuance_date: Date.today, invoice_value: 190.99, increase: 0, scanning_date: Date.today,
+             description: 'Nada', account_plan_id: 1,cost_center_id: 2, discount: 0, net_value: 0)
