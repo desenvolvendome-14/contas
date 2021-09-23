05732516aa9a10e6e48c0864a51f47299b5775d6
Lukas Pol <lukaspaespol@gmail.com>
2021-09-23 06:19:32 -0300

    test(bill_routing): add test for routes

---
+++ spec/routing/api/v1/bills_routing_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe Api::V1::BillsController, type: :routing do
+  describe "routing" do
+    it "routes to #index" do
+      expect(get: "/api/v1/bills").to route_to("api/v1/bills#index")
+    end
+
+    it "routes to #show" do
+      expect(get: "/api/v1/bills/1").to route_to("api/v1/bills#show", id: "1")
+    end
+
+    it "routes to #destroy" do
+      expect(delete: "/api/v1/bills/1").to route_to("api/v1/bills#destroy", id: "1")
+    end
+  end
+end
