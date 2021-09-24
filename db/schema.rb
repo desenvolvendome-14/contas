8c1a1bac4f89f2439744eb23e47ce083cb8a2b35
Lukas Pol <lukaspaespol@gmail.com>
2021-09-24 06:18:32 -0300

    chore: update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_15_085132) do
+ActiveRecord::Schema.define(version: 2021_09_17_085014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 2021_09_15_085132) do
     t.float "discount"
     t.float "net_value"
     t.integer "salesman_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reason_bearishes", force: :cascade do |t|
+    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
