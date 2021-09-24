8c7e52f2088e4b09aa2db07b55519cc0152dd569
Lukas Pol <lukaspaespol@gmail.com>
2021-09-24 06:13:51 -0300

    refactor: rubocop Works

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,13 +8,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Bill.create!(bill_type: :invoice_receivable, company_id: 1, invoice: 'fatura a receber', type_invoice: :bill_safe,
+Bill.create!(bill_type: :invoice_receivable, company_id: 1, invoice: "fatura a receber", type_invoice: :bill_safe,
              issuance_date: Date.today, invoice_value: 150.99, increase: 0, salesman_id: 1, commission: 10)
 
-Bill.create!(bill_type: :invoice_payable, company_id: 2, invoice: 'fatura a pagar', type_invoice: :bill_safe,
+Bill.create!(bill_type: :invoice_payable, company_id: 2, invoice: "fatura a pagar", type_invoice: :bill_safe,
              issuance_date: Date.today, invoice_value: 290.99, increase: 0, scanning_date: Date.today,
-             description: 'Nada', account_plan_id: 1,cost_center_id: 2, discount: 0, net_value: 0)
+             description: "Nada", account_plan_id: 1, cost_center_id: 2, discount: 0, net_value: 0)
 
-Bill.create!(bill_type: :expense_payable, company_id: 2, invoice: 'despesas a pagar', type_invoice: :bill_safe,
+Bill.create!(bill_type: :expense_payable, company_id: 2, invoice: "despesas a pagar", type_invoice: :bill_safe,
              issuance_date: Date.today, invoice_value: 190.99, increase: 0, scanning_date: Date.today,
-             description: 'Nada', account_plan_id: 1,cost_center_id: 2, discount: 0, net_value: 0)
+             description: "Nada", account_plan_id: 1, cost_center_id: 2, discount: 0, net_value: 0)
