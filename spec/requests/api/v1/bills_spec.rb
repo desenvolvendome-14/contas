5b99ffc9c347f878bb422a1d77b5aa4160d7c537
Lukas Pol <lukaspaespol@gmail.com>
2021-09-24 05:39:54 -0300

    test(bill_requests): create test for controller

--- spec/requests/api/v1/bills_spec.rb
+++ spec/requests/api/v1/bills_spec.rb
@@ -1,29 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'API::v1::Bills', type: :request do
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  let(:invoice_receivable_valid_attributes) { build(:invoice_receivable).attributes }
+  let(:invoice_payable_valid_attributes) { build(:invoice_payable).attributes }
+  let(:expense_payable_valid_attributes) { build(:expense_payable).attributes }
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  # This should return the minimal set of values that should be in the headers
-  # in order to pass any filters (e.g. authentication) defined in
-  # Api::V1::BillsController, or in your router and rack
-  # middleware. Be sure to keep this updated too.
-  let(:valid_headers) {
-    {}
-  }
-
-  before :each do
-    create_list(:invoice_receivable, 2)
-    create_list(:invoice_payable, 3)
-    create_list(:expense_payable,4)
+  let(:invalid_attributes) do
+    { bill_type: nil }
   end
 
   describe "GET /index" do
+    before :each do
+      create_list(:invoice_receivable, 2)
+      create_list(:invoice_payable, 3)
+      create_list(:expense_payable, 4)
+    end
     it "invoice_receivable" do
       get api_v1_bills_url, as: :json
       expect(response).to be_successful
@@ -32,9 +23,10 @@ RSpec.describe 'API::v1::Bills', type: :request do
 
   describe "GET /show" do
     it "renders a successful response" do
+      create_list(:invoice_receivable, 2)
       get api_v1_bill_url(Bill.first), as: :json
       expect(response).to be_successful
-      expect(body_json.id).to eq(Bill.first)
+      expect(body_json.id).to eq(Bill.first.id)
       expect(body_json.to_h.keys).to eq([:id, :bill_type, :company_id, :invoice, :type_invoice, :issuance_date,
                                          :invoice_value, :increase, :scanning_date, :description, :account_plan_id,
                                          :cost_center_id, :discount, :net_value, :salesman_id, :commission,
@@ -42,45 +34,75 @@ RSpec.describe 'API::v1::Bills', type: :request do
     end
   end
 
-  # describe "POST /create" do
-  #   context "with valid parameters" do
-  #     it "creates a new Api::V1::Bill" do
-  #       expect {
-  #         post api_v1_bills_url,
-  #              params: { api/v1_bill: valid_attributes }, headers: valid_headers, as: :json
-  #       }.to change(Api::V1::Bill, :count).by(1)
-  #     end
-  #
-  #     it "renders a JSON response with the new api/v1_bill" do
-  #       post api_v1_bills_url,
-  #            params: { api/v1_bill: valid_attributes }, headers: valid_headers, as: :json
-  #       expect(response).to have_http_status(:created)
-  #       expect(response.content_type).to match(a_string_including("application/json"))
-  #     end
-  #   end
-  #
-  #   context "with invalid parameters" do
-  #     it "does not create a new Api::V1::Bill" do
-  #       expect {
-  #         post api_v1_bills_url,
-  #              params: { api/v1_bill: invalid_attributes }, as: :json
-  #       }.to change(Api::V1::Bill, :count).by(0)
-  #     end
-  #
-  #     it "renders a JSON response with errors for the new api/v1_bill" do
-  #       post api_v1_bills_url,
-  #            params: { api/v1_bill: invalid_attributes }, headers: valid_headers, as: :json
-  #       expect(response).to have_http_status(:unprocessable_entity)
-  #       expect(response.content_type).to eq("application/json")
-  #     end
-  #   end
-  # end
+  describe "POST /create_receivable" do
+    context "::Invoice Receivable" do
+      it "creates a new" do
+        expect do
+          post create_receivable_api_v1_bills_url,
+               params: { bill: invoice_receivable_valid_attributes }, as: :json
+        end.to change(Bill.where(bill_type: :invoice_receivable), :count).by(1)
+
+        expect(response).to have_http_status(:created)
+      end
+
+      it "not creates a new with invalid_params" do
+        expect do
+          post create_receivable_api_v1_bills_url,
+               params: { bill: invalid_attributes }, as: :json
+        end.to change(Bill.where(bill_type: :invoice_receivable), :count).by(0)
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe "POST /create_payable" do
+    context "::invoice_payable" do
+      it "creates a new" do
+        expect do
+          post create_payable_api_v1_bills_url,
+               params: { bill: invoice_payable_valid_attributes }, as: :json
+        end.to change(Bill.where(bill_type: :invoice_payable), :count).by(1)
+
+        expect(response).to have_http_status(:created)
+      end
+
+      it "not creates a new with invalid_params" do
+        expect do
+          post create_payable_api_v1_bills_url,
+               params: { bill: invalid_attributes }, as: :json
+        end.to change(Bill.where(bill_type: :invoice_payable), :count).by(0)
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context "::expense_payable" do
+      it "creates a new" do
+        expect do
+          post create_payable_api_v1_bills_url,
+               params: { bill: expense_payable_valid_attributes }, as: :json
+        end.to change(Bill.where(bill_type: :expense_payable), :count).by(1)
+
+        expect(response).to have_http_status(:created)
+      end
+
+      it "not creates a new with invalid_params" do
+        expect do
+          post create_payable_api_v1_bills_url,
+               params: { bill: invalid_attributes }, as: :json
+        end.to change(Bill.where(bill_type: :expense_payable), :count).by(0)
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
 
   describe "DELETE /destroy" do
     it "destroys the requested api/v1_bill" do
       bill = create(:expense_payable)
       expect {
-        delete api_v1_bill_url(bill), headers: valid_headers, as: :json
+        delete api_v1_bill_url(bill), as: :json
       }.to change(Bill, :count).by(-1)
     end
   end
