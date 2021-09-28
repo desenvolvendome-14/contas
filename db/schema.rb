6f76712be27a5760cfee3f0573bf4447ea00466e
Hemershon Silva <hemershon@gmail.com>
2021-09-28 05:18:01 -0300

    feat(schema) add update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_213709) do
+ActiveRecord::Schema.define(version: 2021_09_28_081212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,12 @@ ActiveRecord::Schema.define(version: 2021_09_27_213709) do
     t.float "discount"
     t.float "net_value"
     t.integer "salesman_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "cost_centers", force: :cascade do |t|
+    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
