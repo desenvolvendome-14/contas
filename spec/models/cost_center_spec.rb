a3cfb3993118c058189fdb362a97403fb8cb5977
Hemershon Silva <hemershon@gmail.com>
2021-09-28 05:16:47 -0300

    feat(cost_center) create model in test and validates

--- spec/models/cost_center_spec.rb
+++ spec/models/cost_center_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe CostCenter, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations CostCenter" do
+    it { is_expected.to validate_presence_of :description }
+  end
 end
