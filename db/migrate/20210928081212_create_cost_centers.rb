f5eed9d0543b4a7f1ff716aa6127cbca142810a0
Hemershon Silva <hemershon@gmail.com>
2021-09-29 05:19:58 -0300

    refactor(cosr_center) rule rubocop

--- db/migrate/20210928081212_create_cost_centers.rb
+++ db/migrate/20210928081212_create_cost_centers.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateCostCenters < ActiveRecord::Migration[6.1]
   def change
     create_table :cost_centers do |t|
--- spec/factories/cost_centers.rb
+++ spec/factories/cost_centers.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :cost_center do
     description { "MyString" }
