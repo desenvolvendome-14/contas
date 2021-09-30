ba91756c3c8563dd3217fe0a65897f6cfe6c5d20
Lukas Pol <lukaspaespol@gmail.com>
2021-09-30 06:06:04 -0300

    feat: create participant

---
+++ db/migrate/20210930085216_create_participants.rb
@@ -0,0 +1,10 @@
+class CreateParticipants < ActiveRecord::Migration[6.1]
+  def change
+    create_table :participants do |t|
+      t.string :name
+      t.integer :type
+
+      t.timestamps
+    end
+  end
+end
