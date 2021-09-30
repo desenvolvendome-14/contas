2a708ffddc6890dc297f73770e62c4478b37dc9e
Hemershon Silva <hemershon@gmail.com>
2021-09-30 09:14:35 -0300

    feat(rateio) test

--- spec/models/rateio_spec.rb
+++ spec/models/rateio_spec.rb
@@ -1,10 +1,11 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe Rateio, type: :model do
   describe "Validations" do
     it { is_expected.to validate_presence_of :code }
     it { is_expected.to validate_presence_of :story }
     it { is_expected.to validate_presence_of :value }
-
   end
 end
