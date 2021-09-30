74f9a3e90f4173d2422087888b9507dfee635fed
Hemershon Silva <hemershon@gmail.com>
2021-09-30 06:56:13 -0300

    feat(rateio) build validates and test

--- app/models/rateio.rb
+++ app/models/rateio.rb
@@ -1,6 +1,10 @@
 class Rateio < ApplicationRecord
   belongs_to :account_plan
   belongs_to :cost_center
-  belongs_to :bio_id
-  belongs_to :invoice
+  belongs_to :bill
+
+  validates :code, :story, presence: true
+  validates :account_plan_id, :cost_center_id, :bill, presence: true
+  validates :value, presence: true
+
 end
