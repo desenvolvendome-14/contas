b7227f83957ff8c60a397263ce0f2741eaed199a
Lukas Pol <lukaspaespol@gmail.com>
2021-10-07 05:57:18 -0300

    test: change type to person_type, and create traits

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -5,6 +5,6 @@ require "rails_helper"
 RSpec.describe Participant, type: :model do
   describe "validations" do
     it { is_expected.to validate_presence_of :name }
-    it { is_expected.to validate_presence_of :type }
+    it { is_expected.to validate_presence_of :person_type }
   end
 end
