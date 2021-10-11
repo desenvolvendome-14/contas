499be464c6d1dbdf63cd6333b28e99b704505290
Paulo Prata <paulodanielprata@gmail.com>
2021-10-11 06:46:03 -0300

    feat: creat validates due_date and value positive

--- app/models/installment.rb
+++ app/models/installment.rb
@@ -2,4 +2,22 @@ class Installment < ApplicationRecord
   belongs_to :bill
   belongs_to :account_bank
   belongs_to :type_charge
+
+
+	validates :bill, :account_bank, :type_charge, presence: true
+  validate :verify_date
+	validate :positive_value
+	
+	def verify_date
+		if Date.today > due_date
+			errors.add(:due_date, :verify_date, message: "data vencimento não pode ser maior que data atual")
+		end
+	end
+
+	def positive_value
+		if value <= 0
+			errors.add(:value, :positive_value, message: "Valor não pode ser menor ou igual a zero") 
+		end
+	end
+
 end
