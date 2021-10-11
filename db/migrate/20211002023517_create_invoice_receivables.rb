6e04ce3ebcf8c24f43ed441cc0d9c5a20e2e165f
Hemershon Silva <hemershon@gmail.com>
2021-10-11 06:27:57 -0300

    feat(ratios) create controller and test at endpoint

--- db/migrate/20211002023517_create_invoice_receivables.rb
+++ db/migrate/20211002023517_create_invoice_receivables.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateInvoiceReceivables < ActiveRecord::Migration[6.1]
   def change
     create_table :invoice_receivables do |t|
--- spec/routing/api/v1/rateios_routing_spec.rb
+++ spec/routing/api/v1/rateios_routing_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 RSpec.describe Api::V1::RateiosController, type: :routing do
@@ -9,7 +11,6 @@ RSpec.describe Api::V1::RateiosController, type: :routing do
     it "routes to #show" do
       expect(get: "/api/v1/rateios/1").to route_to("api/v1/rateios#show", id: "1")
     end
-
 
     it "routes to #create" do
       expect(post: "/api/v1/rateios").to route_to("api/v1/rateios#create")
