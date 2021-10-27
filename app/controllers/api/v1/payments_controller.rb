73d8e8cc09bacdcfda73d1ea86ba3175141dc1c8
Lukas Pol <lukaspaespol@gmail.com>
2021-10-27 07:08:07 -0300

    fix: change name params permit

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -16,7 +16,7 @@ module Api
       def payment_params
         params.require(:payment).permit(
           :pay_date, :accounting_date, :amount_paid,
-          :discount_paid, :interest_amount, :increase_amount,
+          :discount_amount, :interest_amount, :increase_amount,
           :total_amount, :charts_accounts_amount_paid_id,
           :charts_accounts_discount_amount_id, :charts_accounts_interest_amount_id,
           :charts_accounts_increase_amount_id, :bill_id, :installment_id,
