3cd80b578553c09b41e72f752908162d0f03da82
Paulo Prata <paulodanielprata@gmail.com>
2021-10-29 07:18:26 -0300

    feat(instalment) create validate

--- app/models/installment.rb
+++ app/models/installment.rb
@@ -7,7 +7,13 @@ class Installment < ApplicationRecord
 	validates :bill, :account_bank, :type_charge, :due_date, presence: true
   validate :verify_date
 	validate :positive_value
+	validate :verify_discount, if: :receivable?
 	
+  def receivable?
+		self.bill.invoice_receivable?
+		
+	end
+
 	def verify_date
 		if due_date.nil? || Date.today > due_date
 			errors.add(:due_date, :verify_date, message: "data vencimento não pode ser maior que data atual")
@@ -17,6 +23,11 @@ class Installment < ApplicationRecord
 	def positive_value
 		if value.nil? || value <= 0
 			errors.add(:value, :positive_value, message: "Valor não pode ser menor ou igual a zero") 
+		end
+	end
+	def  verify_discount
+		if discount.nil? || discount < 0
+			errors.add(:discount, :verify_discount, message: "Valor do Desconto não pode ser menor que zero")  
 		end
 	end
 
