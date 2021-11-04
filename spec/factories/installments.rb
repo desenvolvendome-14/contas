7901514bfda565de334d98997f4527616ae251ac
Paulo Prata <paulodanielprata@gmail.com>
2021-11-04 06:24:27 -0300

    refactor:  reajust rubocop

--- spec/factories/installments.rb
+++ spec/factories/installments.rb
@@ -9,15 +9,15 @@ FactoryBot.define do
     type_charge { create(:type_charge) }
 
     trait :installment_receivable do
-      discount_due_date {Date.tomorrow}
-      discount {8.09}
-      notary_title {"MyString"}
+      discount_due_date { Date.tomorrow }
+      discount { 8.09 }
+      notary_title { "MyString" }
       send_date { Date.tomorrow }
-      notary_value {1.60}
-      protested_title {"MyString"}
-      protest_date {Date.tomorrow}
-      protest_value {3.50}
-      bank_remittance {"MyString"}
+      notary_value { 1.60 }
+      protested_title { "MyString" }
+      protest_date { Date.tomorrow }
+      protest_value { 3.50 }
+      bank_remittance { "MyString" }
       bill { create(:invoice_receivable) }
     end
   end
