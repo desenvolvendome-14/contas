21d012b9467d2fafb80994d93185bc346f0e7fb5
Lukas Pol <lukaspaespol@gmail.com>
2021-11-09 06:48:45 -0300

    feat(installment): create list

--- app/controllers/api/v1/installments_controller.rb
+++ app/controllers/api/v1/installments_controller.rb
@@ -1,6 +1,12 @@
 module Api
   module V1
     class InstallmentsController < ApplicationController
+      before_action :set_bill, only: :index
+
+      def index
+        @installments = @bill.installments
+      end
+
       def create
         @installment = Installment.new(installment_params)
 
@@ -12,6 +18,14 @@ module Api
       end
 
       private
+
+      def set_bill
+        @bill = Bill.find(params[:bill_id])
+      rescue StandardError => e
+        render json: {
+          message: 'Deve enviar o paramentro `bill_id` válido'
+        }, status: :unprocessable_entity
+      end
 
       def installment_params
         params.require(:installment).permit(
