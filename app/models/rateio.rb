d3cda87f9c72f2f5f711db204fc9aedd3cadc59a
Hemershon Silva <hemershon@gmail.com>
2021-11-09 05:38:38 -0300

    feat(ratios) create model and test

--- app/models/rateio.rb
+++ app/models/rateio.rb
@@ -1,3 +1,8 @@
 class Rateio < ApplicationRecord
+  has_many :bills
+  has_many :account_plans
+
+  enum bill_type: [:invoice_receivable, :invoice_payable, :expense_payable]
+
   validates :value_rateio, :obs, presence: true
 end
--- spec/models/rateio_spec.rb
+++ spec/models/rateio_spec.rb
@@ -1,10 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Rateio, type: :model do
   describe "General" do
     context "::Validations" do
-       it { is_expected.to validate_presence_of :value_rateio }
+      it { is_expected.to validate_presence_of :value_rateio }
     end
-
   end
 end
