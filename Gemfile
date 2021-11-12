23ffbe986e6836ebb773cfc29b9eeef918caf11f
Hemershon Silva <hemershon@gmail.com>
2021-11-12 09:13:00 -0300

    refactor(seed) add times and create

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem "annotate", git: "https://github.com/ctran/annotate_models.git"
   gem "spring"
+  gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 end
 
 group :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Company.create!([{ name: "Google" }, { name: "Amazon" }])
+Company.create!([{ name: Faker::Company.name }])
 
+10.times do |i|
+  Participant.create(name: Faker::Name.name, person_type: :salesman)
+end
 Participant.create(name: "Fulano", person_type: :salesman)
 Participant.create(name: "Beltrano", person_type: :client)
 Participant.create(name: "Deltrano", person_type: :company)
 
-ChartAccounts.create([{ description: "account 1" }, { description: "account 2" }])
+10.times do |i|
+  ChartAccounts.create([{ description: "account #{i}" }])
+end
 
-CostCenter.create([{ description: "Cost CEnter 1" }, { description: "Cost CEnter 1" }])
+10.times do |i|
+  CostCenter.create([{ description: "Cost CEnter #{i}" }])
+end
 
-AccountBank.create(description: "Conta Banco do Brasil")
-AccountBank.create(description: "Conta Caixa Federal")
+10.times do |i|
+  AccountBank.create(description: Faker::Bank.name)
+end
 
 TypeCharge.create([{ description: "Carteira Simples" }, { description: "Carteira Registrada" }])
 
