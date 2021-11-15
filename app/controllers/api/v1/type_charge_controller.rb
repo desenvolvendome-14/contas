ad11185cecdb92f5c4055078e3216e5d655f53b7
Hemershon Silva <hemershon@gmail.com>
2021-11-15 14:53:35 -0300

    feat(type_charge) list description

--- app/controllers/api/v1/type_charge_controller.rb
+++ app/controllers/api/v1/type_charge_controller.rb
@@ -1,11 +1,10 @@
 module Api
   module V1
     class TypeChargeController < ApplicationController
-        before_action :set_typeCharge, only: %i[show update destroy]
-        def index
-          @type_charge = TypeCharge.all
-        end
+      before_action :set_typeCharge, only: %i[show update destroy]
+      def index
+        @type_charge = TypeCharge.all
+      end
     end
   end
 end
-
