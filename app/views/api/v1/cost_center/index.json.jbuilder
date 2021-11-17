ed566a7f269e705d25819c1d7d2145faec0997a1
Hemershon Silva <hemershon@gmail.com>
2021-11-16 22:24:02 -0300

    Revert "Feature/list cost center (#68)"
    This reverts commit 8969e25a7ef36b37134ee5297439f631910f9e8f.

--- app/views/api/v1/cost_center/index.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.array! @cost_center, :id, :description
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,6 @@ Rails.application.routes.draw do
       resources :document_types
       resources :reason_bearishes
       resources :rateios, only: [:index ]
-      resources :cost_center
       resources :payments do
         post :create_payable, on: :collection
         post :create_invoice_receivable, on: :collection
