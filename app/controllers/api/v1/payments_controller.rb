7ce63890564b146497445caa24c142a99827add1
Lukas Pol <lukaspaespol@gmail.com>
2021-11-18 06:27:10 -0300

    feat(payment): add show

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -1,7 +1,9 @@
 module Api
   module V1
     class PaymentsController < ApplicationController
-      before_action :set_payment, only: %i[update destroy]
+      before_action :set_payment, only: %i[show update destroy]
+
+      def show; end
 
       def create
         @payment = Payment.new(payment_params)
